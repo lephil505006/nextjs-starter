cb9526e5d66f328f2abbe9e55f159445f7b554fc
Philip Le <lephil505006@gmail.com>
2023-11-30 15:43:16 -0600

    Adding finishes for info pages

--- pages/info.js
+++ pages/info.js
@@ -9,7 +9,9 @@ const InfoPage = () => {
   return (
     <div className="info-container">
       <div className="info-text">
-        <h1>More about Exeggutor</h1>
+        <h1 className="bg-green-500 p-8 px-20 rounded-md">
+          More about Exeggutor
+        </h1>
         <p>
           This pokemon is considered a &quot;Grass&quot; and &quot;Psychic&quot;
         </p>
@@ -17,12 +19,16 @@ const InfoPage = () => {
           However, this pokemon has a 2nd version which is the alolan verison
         </p>
         <p>
-          Which makes the Exeggutor a &quot;Grass&quot; &quot;Dragon&quot;
+          Which makes the Exeggutor a &quot;Grass&quot; and &quot;Dragon&quot;
           pokemon instead.
         </p>
         <br />
+        <p>The data retrieved for Exeggutor was from PokeAPI</p>
+        <a className="back font-bold" href="https://pokeapi.co/">
+          Go to PokeAPI.com
+        </a>
         <Link href="./">
-          <p className="back font-bold rounded-md">Main Page</p>
+          <p className="back font-bold rounded-md bg-red-500">Main Page</p>
         </Link>
       </div>
       <div>
