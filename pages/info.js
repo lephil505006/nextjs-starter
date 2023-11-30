5880114f5dcfe0bf50d6c563db59c5be5bdbc442
Philip Le <lephil505006@gmail.com>
2023-11-30 13:43:58 -0600

    Fixing pretteir error

--- pages/info.js
+++ pages/info.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Link from 'next/link';
 
 const InfoPage = () => {
+  const imageUrl = `
+    https://archives.bulbagarden.net/media/upload/9/94/0103Exeggutor-Alola.png
+  `;
+
   return (
     <div>
       <h1>More about Exeggutor</h1>
       <Link href="./">
         <p>Back</p>
       </Link>
-      <br></br>
+      <br />
       <p>This is the information page.</p>
       <img
         className="alolan"
-        src={'https://archives.bulbagarden.net/media/upload/9/94/0103Exeggutor-Alola.png'}
+        src={imageUrl}
         alt="alolanegg"
       />
     </div>
